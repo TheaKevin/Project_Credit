5b6c348bdc085a4e874d285375fd43fca26ff002
TheaKevin <theakevin01@gmail.com>
2023-01-03 17:43:10 +0700

    database done

--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"net/http"
-	"os"
+	"project_credit_sinarmas/backend/api"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
-func handler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hello world",
-	})
-}
-
 func main() {
-	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-	}))
-	r.GET("/", handler)
-	r.Run(":" + os.Getenv("PORT"))
+	db, err := api.SetupDb()
+	if err != nil {
+		panic(err)
+	}
+
+	server := api.MakeServer(db)
+	server.RunServer()
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const getData = async () => {
     try {
-      const res = await fetch('http://localhost:8080/');
+      const res = await fetch("http://localhost:8080");
       const data = await res.json();
       setData(data);
     }
