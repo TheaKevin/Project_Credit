ecaf887f3d4140a31949e44ef6e74a14d5f441b6
TheaKevin <theakevin01@gmail.com>
2023-01-06 14:14:22 +0700

    service staging_customer done

--- backend/controllers/stagingCustomer/repository.go
+++ backend/controllers/stagingCustomer/repository.go
@@ -22,6 +22,25 @@ func NewRepository(db *gorm.DB) *repository {
 	return &repository{db}
 }
 
+func (r *repository) validationFailed(id int64, reason string, item models.StagingCustomer) {
+	var sc []models.StagingCustomer
+
+	// change stagingCustomer sc_flag to 8
+	r.db.Model(&sc).Where("id = ?", id).Update("sc_flag", "8")
+
+	//insert to stagingError
+	stagingError := models.StagingError{
+		SeReff:       item.ScReff,
+		SeCreateDate: item.ScCreateDate,
+		BranchCode:   item.ScBranchCode,
+		Company:      item.ScCompany,
+		Ppk:          item.CustomerPpk,
+		Name:         item.CustomerName,
+		ErrorDesc:    reason,
+	}
+	r.db.Create(&stagingError)
+}
+
 func (r *repository) GetStagingCustomer() ([]models.StagingCustomer, error) {
 	var sc []models.StagingCustomer
 	var c []models.CustomerDataTab
@@ -40,24 +59,21 @@ func (r *repository) GetStagingCustomer() ([]models.StagingCustomer, error) {
 		// validasi ppk duplikasi
 		res2 := r.db.Where("ppk = ?", item.CustomerPpk).First(&c)
 		if res2.Error == nil {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " ppk duplicate")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "CUSTOMER_PPK tidak boleh duplikasi pada table Customer_data_tab", item)
 			continue
 		}
 
 		// validasi SC_Company terdaftar
 		res2 = r.db.Where("company_short_name = ?", item.ScCompany).First(&com)
 		if res2.Error != nil {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " sc_company gk ad")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "SC_COMPANY harus terdaftar di Tabel Mst_Company_Tab", item)
 			continue
 		}
 
 		// validasi SC_BranchCode terdaftar
 		res2 = r.db.Where("code = ?", item.ScBranchCode).First(&b)
 		if res2.Error != nil {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " branch code gk ad")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "C_BRANCH_CODE harus terdaftar di Tabel Branch_Tab", item)
 			continue
 		}
 
@@ -67,66 +83,57 @@ func (r *repository) GetStagingCustomer() ([]models.StagingCustomer, error) {
 			fmt.Println(err)
 		}
 		if (int(t.Month()) != int(date.Month())) || (int(t.Year()) != int(date.Year())) {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " bulan pk berbeda dengan bulan berjalan")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "TGL_PK / DRAWDOWN_DATE tidak boleh berbeda bulan dengan bulan berjalan saat in", item)
 			continue
 		}
 
 		// validasi CustomerIDType & CustomerIDNumber
 		if item.CustomerIDType == "1" && item.CustomerIDNumber == "" {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " customer id number gk ad")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "Jika 'CUSTOMER_ID_TYPE' diisi = 1, maka 'CUSTOMER_ID_NUMBER' harus diisi dan tidak boleh kosong", item)
 			continue
 		}
 
 		// validasi Nama Debitur no char spesial
 		if strings.ContainsAny(item.CustomerName, "!@#$%^&*()_+-=[]{}|\\:;'\"<>,./?") {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " nama customer terdapat special character")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "NAMA Debitur tidak boleh mengandung karakter special", item)
 			continue
 		}
 
 		// validasi Vehicle_BPKB kosong
 		if item.VehicleBpkb == "" {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " vehicle_bpkb gk ad")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "VEHICLE_BPKB tidak boleh kosong", item)
 			continue
 		}
 
 		// validasi Vehicle_STNK kosong
 		if item.VehicleStnk == "" {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " vehicle_stnk gk ad")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "VEHICLE_STNK tidak boleh kosong", item)
 			continue
 		}
 
 		// validasi Vehicle_Engine_NO kosong
 		if item.VehicleEngineNo == "" {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " vehicle_engine_no gk ad")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "VEHICLE_ENGINE_NO tidak boleh kosong", item)
 			continue
 		}
 
 		// validasi Vehicle_Chasis_NO kosong
 		if item.VehicleChasisNo == "" {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " vehicle_chasis_no gk ad")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "VEHICLE_CHASIS_NO tidak boleh kosong", item)
 			continue
 		}
 
 		// validasi Vehicle_Engine_NO Duplikasi
 		res2 = r.db.Where("engine_no = ?", item.VehicleEngineNo).First(&v)
 		if res2.Error == nil {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " engine no duplikasi")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "VEHICLE_ENGINE_NO tidak boleh duplikasi pada table 'Vihicle_data_Tab'", item)
 			continue
 		}
 
 		// validasi Vehicle_Chasis_No Duplikasi
 		res2 = r.db.Where("chasis_no = ?", item.VehicleChasisNo).First(&v)
 		if res2.Error == nil {
-			fmt.Println(strconv.FormatInt(item.ID, 10) + " chasis no duplikasi")
-			r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "8")
+			r.validationFailed(item.ID, "VEHICLE_CHASIS_NO tidak boleh duplikasi pada table 'Vihicle_data_Tab'", item)
 			continue
 		}
 
@@ -186,21 +193,6 @@ func (r *repository) GetStagingCustomer() ([]models.StagingCustomer, error) {
 		r.db.Create(&customer)
 
 		// Convert type for input to loanData
-		LoanOtr, err := strconv.ParseFloat(item.LoanOtr, 64)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		LoanDownPayment, err := strconv.ParseFloat(item.LoanDownPayment, 64)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		LoanLoanAmountChanneling, err := strconv.ParseFloat(item.LoanLoanAmountChanneling, 64)
-		if err != nil {
-			fmt.Println(err)
-		}
-
 		LoanInterestFlatChanneling, err := strconv.ParseFloat(item.LoanInterestFlatChanneling, 32)
 		if err != nil {
 			fmt.Println(err)
@@ -216,25 +208,20 @@ func (r *repository) GetStagingCustomer() ([]models.StagingCustomer, error) {
 			fmt.Println(err)
 		}
 
-		LoanMonthlyPaymentChanneling, err := strconv.ParseFloat(item.LoanMonthlyPaymentChanneling, 64)
-		if err != nil {
-			fmt.Println(err)
-		}
-
 		// insert to loanData
 		loan := models.LoanDataTab{
 			Custcode:             newCustCode,
 			Branch:               item.ScBranchCode,
-			OTR:                  LoanOtr,
-			DownPayment:          LoanDownPayment,
-			LoanAmount:           LoanLoanAmountChanneling,
+			OTR:                  item.LoanOtr,
+			DownPayment:          item.LoanDownPayment,
+			LoanAmount:           item.LoanLoanAmountChanneling,
 			LoanPeriod:           item.LoanLoanPeriodChanneling,
 			InterestType:         0,
 			InterestFlat:         float32(LoanInterestFlatChanneling),
 			InterestEffective:    float32(LoanInterestEffectiveChanneling),
 			EffectivePaymentType: int8(LoanEffectivePaymentType),
-			AdminFee:             0,
-			MonthlyPayment:       LoanMonthlyPaymentChanneling,
+			AdminFee:             "0",
+			MonthlyPayment:       item.LoanMonthlyPaymentChanneling,
 			InputDate:            t,
 			LastModified:         t,
 			ModifiedBy:           "",
@@ -244,6 +231,77 @@ func (r *repository) GetStagingCustomer() ([]models.StagingCustomer, error) {
 			ModifiedBy2:          "",
 		}
 		r.db.Create(&loan)
+
+		// Convert type for input to vehicleData
+		VehicleBrand, err := strconv.ParseInt(item.VehicleBrand, 10, 8)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		VehicleStatus, err := strconv.ParseInt(item.VehicleStatus, 10, 8)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		VehicleDealerID, err := strconv.ParseInt(item.VehicleDealerID, 10, 8)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		VehicleTglStnk, err := time.Parse("2006-01-02 15:04:05", item.VehicleTglStnk)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		VehicleTglBpkb, err := time.Parse("2006-01-02 15:04:05", item.VehicleTglBpkb)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		CollateralTypeID, err := strconv.ParseInt(item.CollateralTypeID, 10, 8)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// insert to vehicleData
+		vehicle := models.VehicleDataTab{
+			Custcode:       newCustCode,
+			Brand:          int(VehicleBrand),
+			Type:           item.VehicleType,
+			Year:           item.VehicleYear,
+			Golongan:       1,
+			Jenis:          item.VehicleJenis,
+			Status:         int8(VehicleStatus),
+			Color:          item.VehicleColor,
+			PoliceNo:       item.VehiclePoliceNo,
+			EngineNo:       item.VehicleEngineNo,
+			ChasisNo:       item.VehicleChasisNo,
+			Bpkb:           item.VehicleBpkb,
+			RegisterNo:     "1",
+			Stnk:           item.VehicleStnk,
+			StnkAddress1:   "",
+			StnkAddress2:   "",
+			StnkCity:       "",
+			DealerID:       int(VehicleDealerID),
+			Inputdate:      t,
+			Inputby:        "system",
+			Lastmodified:   t,
+			Modifiedby:     "system",
+			TglStnk:        VehicleTglStnk,
+			TglBpkb:        VehicleTglBpkb,
+			TglPolis:       t,
+			PolisNo:        item.VehiclePoliceNo,
+			CollateralID:   CollateralTypeID,
+			Ketagunan:      "",
+			AgunanLbu:      "",
+			Dealer:         item.VehicleDealer,
+			AddressDealer1: item.VehicleAddressDealer1,
+			AddressDealer2: item.VehicleAddressDealer2,
+			CityDealer:     item.VehicleCityDealer,
+		}
+		r.db.Create(&vehicle)
+
+		r.db.Model(&sc).Where("id = ?", item.ID).Update("sc_flag", "1")
 	}
 
 	return sc, nil
